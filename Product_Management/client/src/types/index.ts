a668f9809224fcba762a8a36367e07eb67864dd0
Ringor123 <ringor123@gmail.com>
2025-03-12 17:14:23 +0100

    removed product management app

--- Product_Management/client/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-import {z} from 'zod'
-
-export const newProductSchema = z.object({
-    name: z.string(),
-    price: z.number()
-})
-
-export type NewProductType = z.infer<typeof newProductSchema>
